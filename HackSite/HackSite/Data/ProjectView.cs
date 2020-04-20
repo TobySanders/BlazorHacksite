aab8916d1c020e2a354b1d87be78c979fd0d666b
TobySanders <tobysanders18@gmail.com>
2020-04-20 17:55:53 +0100

    Changed applicaiton to be blazor server instead of webassembly
    added a dummy teams implementaiton for demo purposes

---
+++ HackSite/HackSite/Data/ProjectView.cs
@@ -0,0 +1,9 @@
+﻿namespace HackSite.Data
+{
+    public class ProjectView
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ HackSite/HackSite/Mappers/ViewMappers.cs
@@ -0,0 +1,28 @@
+﻿using HackSite.Data;
+using UserManagement.Abstractions.Models;
+
+namespace HackSite.Mappers
+{
+    public static class ViewMappers
+    {
+        public static TeamView Map(this Team team)
+        {
+            return new TeamView
+            {
+                Id = team.Id,
+                Name = team.Name,
+                project = team.project.Map()
+            };
+        }
+
+        public static ProjectView Map(this Project project)
+        {
+            return new ProjectView
+            {
+                Id = project.Id,
+                Title = project.Title,
+                Description = project.Description
+            };
+        }
+    }
+}
