9e3fef2ee1fb54fdb8fa583bc467b90eea3ab518
Selim S. Balci <16270624+selimser@users.noreply.github.com>
2020-04-26 21:46:15 +0100

    Summaries for the project object

--- HackSite/UserManagement.Abstractions/Models/Project.cs
+++ HackSite/UserManagement.Abstractions/Models/Project.cs
@@ -4,12 +4,39 @@ namespace UserManagement.Abstractions.Models
 {
     public class Project
     {
+        /// <summary>
+        /// The Id of the project
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// The title / name of the project
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Description of the project
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// The repository URL that contains the source of the project
+        /// </summary>
         public string RepositoryUrl { get; set; }
-        public string ProjectImage { get; set; } //base64
+
+        /// <summary>
+        /// Project image in base64 format
+        /// </summary>
+        public string ProjectImage { get; set; }
+
+        /// <summary>
+        /// Short code of the project which is used for "quick join" functionality"
+        /// </summary>
         public string ShortCode { get; set; } //For sharing
+
+        /// <summary>
+        /// Specifies if the project is an active or completed project
+        /// </summary>
         public bool IsActive { get; set; }
     }
 }
