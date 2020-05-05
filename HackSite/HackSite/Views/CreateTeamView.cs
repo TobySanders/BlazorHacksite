119ff50614aec6b08048f186f0596d7337b8bcce
TobySanders <tobysanders18@gmail.com>
2020-05-05 10:38:49 +0100

    Added some team creation components

---
+++ HackSite/HackSite/Views/CreateTeamView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackSite.Views
+{
+    public class CreateTeamView
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Guid> ProjectIds { get; set; }
+        public List<Guid> MemberIds { get; set; }
+    }
+}
